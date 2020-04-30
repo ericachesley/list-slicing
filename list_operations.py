7f0e01884e7f8ffd531c243a6515751e13978111
Erica Chesley <erchesley@gmail.com>
2020-04-30 19:25:37 +0000

    All functions completed

--- list_operations.py
+++ list_operations.py
@@ -182,8 +182,8 @@ def delete_third_and_seventh(input_list):
 
     """
 
-    input_list.pop(2)
-    input_list.pop(5)
+    del input_list[2]
+    del input_list[5]
 
 
 def delete_middle(input_list):
